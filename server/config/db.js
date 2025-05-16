036656c35180879fd0d51ca5368f87468f4a1a11
rida-rabeea <rabeearida@gmail.com>
2025-05-16 19:57:02 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 mongoose.set('strictQuery', false);
 
 
-mongoose.connect("mongodb://127.0.0.1:27017/BlogApp").then(()=>{
+mongoose.connect("mongodb+srv://rizwanarrf:6Xbxp2dViPV5WMK7@cluster0.iwjmuxz.mongodb.net/BLOGAPP?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
     console.log("connected!");
 }).catch((err)=>{
     console.log(err);
